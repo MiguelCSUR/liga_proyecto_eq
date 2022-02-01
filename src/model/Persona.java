c6342f57d95770d61d894d558abdbfd1c50e176a
MiguelCSUR <93922638+MiguelCSUR@users.noreply.github.com>
2022-02-01 08:42:29 +0100

    modificada Persona.java
    Lo subo de parte de Artsiom, queda por debuggear edadAsignada() no compila

--- src/model/Persona.java
+++ src/model/Persona.java
@@ -1,34 +1,86 @@
 package model;
 
-public abstract class Persona {
-    private String nombre;
-    private String apellidos;
-    private int edad;
+public class Persona {
+    private String 	nombre;
+    private String 	apellidos;
+    private int 	edad;
 
+    //Constructor
+    public Persona() {
+        this.apellidos = apellidoCreado()+" "+apellidoCreado();
+        //this.nombre =	nombreCreado();
+        this.edad = edadAsignada(apellidos);
+    }
+
+
+    //NOMBRE
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    //APELLIDOS
     public String getApellidos() {
         return apellidos;
     }
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
+
+    public void setApellidos() {
+        this.apellidos = apellidoCreado();
     }
+
+    //EDAD
     public int getEdad() {
         return edad;
     }
-    public void setEdad(int edad) {
+
+    public void setEdad(int edadMax, int edadMin) {
+        int edad = (int)Math.floor(Math.random()*(edadMax-edadMin))+edadMin;
         this.edad = edad;
     }
 
+    //GENERADOR SYSO
     @Override
     public String toString() {
-        return "Nombre: "+ this.nombre+" "+this.apellidos+"\n"+
-                "Edad: "+this.edad;
-
+        return "Nombre: "+this.nombre+" "+this.apellidos+"\n"+"Edad: "+this.edad;
     }
 
+
+
+
+    //Generador de Apellidos aleatorios
+    public static String apellidoCreado () {
+        String apellidos[] = {"Marui", "Shima", "Sawa", "Toyo", "Cromwell", "Keller", "Perdido", "Zimmerman", "Anderson", "Gasper", "Degurechaff", "Jimenez",
+                "Gonzalez", "Lamata", "Feliz","Bragueta", "Amiano", "Enamorado", "Nigote", "Delano", "Messina",
+                "Di Montelroso", "Krauser", "Marston", "Auditore", "Bernstein", "Bogard", "Briggs", "Chaolan", "Eggman", "Dorian", "Dragunov",
+                "Drake", "Winters", "Falcon", "Freeman", "Hudson", "Ibn-La' Ahad", "Yamazaki", "Wesker", "Snake", "Tsung", "Takahashi", "Strife",
+                "Schugerg", "Schtauffen", "Sakazaki", "Hayabusa", "Porter", "Nikaido", "Nanakase", "Chief", "Tyrell", "Lannister", "Baratheon",
+                "Martell", "Stark", "Targaryen", "Tully", "Okabe", "Krusoe", "Vergassola", "Ali Baba", "Serebriakov"};
+
+
+        int numero=(int) Math.floor(Math.random()*apellidos.length)-1;
+        //String apellido = apellidos[numero];
+        return apellidos[numero];
+    }
+
+
+
+    public static String nombreCreado () {
+
+        String[] nombres = {"Altair","Ezio", "Sora", "Connor", "Kratos","Link","Zelda","Kirby","Megaman", "Bowser",
+                "Ratchet","Donkey Kong", "Goku", "Doraemon", "Perchita", "Suneo", "Gandalf", "Frodo", "Bilbo",
+                "Dovahkiin", "Sonic", "Cloud", "Sephiroth", "Dante", "Geralt","Trevor","Victor", "Jinx", "VI", "Catelyn",
+                "Katarina", "Treiny", "Shuna", "Talim", "Ivy", "CJ", "Spyro","Squall","Yuna", "Riku", "Jill", "Wesker",
+                "Kassandra", "Alexios", "Luigi","Cicerón","Duke","Tifa","Alucard","Drake", "Koopa","Axel","Arwen",
+                "Eowyn", "Sakura", "Naruto", "Ash", "Sasuke", "Vegeta", "Sauron","Legolas","Leia","Luke", "Vader","Rosa"};
+
+
+        int numero = (int) Math.floor(Math.random()*nombres.length);
+        String nombre = nombres[numero];
+
+        return nombre[numero];
+
+    }
 }
