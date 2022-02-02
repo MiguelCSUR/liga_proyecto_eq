aef3fd563c5719c8afc87d739fbf4995deb80d96
MiguelCSUR <93922638+MiguelCSUR@users.noreply.github.com>
2022-02-02 17:56:28 +0100

    crear clase Partido.java

---
+++ src/model/Partido.java
@@ -0,0 +1,66 @@
+package model;
+
+public class Partido {
+
+    private Equipo equipoCasa;
+    private Equipo equipoFuera;
+    private Arbitro arbitro;
+    private int golesEquipoCasa;
+    private int golesEquipoFuera;
+
+    public Partido(Equipo equipoCasa, Equipo equipoFuera, Arbitro arbitro) {
+        this.equipoCasa = equipoCasa;
+        this.equipoFuera = equipoFuera;
+        this.arbitro = arbitro;
+    }
+
+    public Equipo getEquipoCasa() {
+        return equipoCasa;
+    }
+
+    public void setEquipoCasa(Equipo equipoCasa) {
+        this.equipoCasa = equipoCasa;
+    }
+
+    public Equipo getEquipoFuera() {
+        return equipoFuera;
+    }
+
+    public void setEquipoFuera(Equipo equipoFuera) {
+        this.equipoFuera = equipoFuera;
+    }
+
+    public Arbitro getArbitro() {
+        return arbitro;
+    }
+
+    public void setArbitro(Arbitro arbitro) {
+        this.arbitro = arbitro;
+    }
+
+    public int getGolesEquipoCasa() {
+        return golesEquipoCasa;
+    }
+
+    public void setGolesEquipoCasa(int golesEquipoCasa) {
+        this.golesEquipoCasa = golesEquipoCasa;
+    }
+
+    public int getGolesEquipoFuera() {
+        return golesEquipoFuera;
+    }
+
+    public void setGolesEquipoFuera(int golesEquipoFuera) {
+        this.golesEquipoFuera = golesEquipoFuera;
+    }
+
+    @Override
+    public String toString() {
+        return "Partido" +
+                "\nequipoCasa=" + equipoCasa.getNombre() +
+                "\nequipoFuera=" + equipoFuera.getNombre() +
+                "\narbitro=" + arbitro.getNombre() + " " +  arbitro.getApellidos() +
+                "\ngolesEquipoCasa=" + golesEquipoCasa +
+                "\ngolesEquipoFuera=" + golesEquipoFuera;
+    }
+}
